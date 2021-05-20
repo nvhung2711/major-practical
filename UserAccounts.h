530e67b8cadd99eb5ac5d4dc20c4c94d913ea430
nvhung2711 <51402887+nvhung2711@users.noreply.github.com>
2021-05-20 11:11:39 +0930

    Update UserAccounts.h

---
+++ UserAccounts.h
@@ -0,0 +1,28 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include <string>
+
+#include "details.h"
+
+class account {
+private:
+    double balance;
+    double current_loan;
+    int credit_score;
+    int interest_rate;
+public:
+    account();
+    void deposit(double amount);
+    void withdraw(double amount);
+    int get_balance();
+    void set_credit_score(int score);
+    int get_credit_score();
+    void set_interest_rate(int rate);
+    int get_interest_rate();
+    int get_current_loan();
+    void pay_loan(double amount);
+    void make_loan(double amount);
+    ~account();
+};
