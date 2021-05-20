fd360a43b4f017d223e33d26f6dcbb7436326270
aryamanm21 <72347074+aryamanm21@users.noreply.github.com>
2021-05-20 11:31:03 +0930

    Update Bank.cpp
    Initial Version Bank.h

--- Bank.cpp
+++ Bank.cpp
@@ -1,1 +1,41 @@
+include "Bank.h"
+#include<iostream>
+
+#include<string>
+
+using namespace std;
+
+// Empty bank with no user accounts
+
+Bank::Bank() : max_size(0), curr_size(0), members(NULL)
+{}
+
+
+// Creating an empty number of user accounts
+Bank::Bank(int size) : max_size(size), curr_size(0)
+
+{
+
+       members = new UserAccount[size];
+
+}
+// Returns no. of user accounts in the banking system
+int Bank::get_current_number_of_accounts()
+
+{
+
+       return curr_size;
+
+}
+
+
+UserAccounts*Bank::get_members()
+
+{
+
+       return members;
+
+}
+
+
 
